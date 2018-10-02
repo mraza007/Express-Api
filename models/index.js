448e184f1476b6114702f9788267a3e2c2051a92
Muhammad <muhammadraza0047@gmail.com>
2018-10-02 14:50:33 -0400

    removed db link

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 mongoose.set('debug',true);
-mongoose.connect('mongodb://raza:raza1234@ds117423.mlab.com:17423/todo-api') 
-mongoose.Promise = Promise; 
+//mongoose.connect('insert mlab database link')
+mongoose.Promise = Promise;
 module.exports.Todo = require("./todo")
